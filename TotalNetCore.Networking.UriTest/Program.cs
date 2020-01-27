0431cab885500f2e9d77ee9dd81c6d6c97c564a2
qdjjx <qdjjx9441@sina.com>
2020-01-27 10:20:15 +0800

    Networking, URI and DNS

---
+++ TotalNetCore.Networking.UriTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace TotalNetCore.Networking.UriTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var simpleUri = GetSimpleUri();
+            Console.WriteLine(simpleUri.ToString());
+            Console.ReadKey();
+            Thread.Sleep(10000);
+        }
+
+        public static Uri GetSimpleUri() {
+            var builder = new UriBuilder();
+            builder.Scheme = "http";
+            builder.Host = "baidu.com";
+            return builder.Uri;
+        }
+
+        public static Uri GetSimpleUri_Constructor() {
+            var builder = new UriBuilder("http", "baidu.com");
+            return builder.Uri;
+        }
+    }
+
+    
+}
---
+++ TotalNetCore.Netwroking.DnsTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace TotalNetCore.Netwroking.DnsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var domainEntry = Dns.GetHostEntry("baidu.com"); //接收域名
+            //Console.WriteLine(domainEntry.HostName);
+            //foreach (var ip in domainEntry.AddressList)
+            //{
+            //    Console.WriteLine(ip);
+            //}
+            //Console.ReadKey();
+
+            var domainEntryByAddress = Dns.GetHostEntry("127.0.0.1");//接收IP
+            Console.WriteLine(domainEntryByAddress.HostName);
+            foreach(var ip in domainEntryByAddress.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+            Thread.Sleep(10000);
+        }
+    }
+}
