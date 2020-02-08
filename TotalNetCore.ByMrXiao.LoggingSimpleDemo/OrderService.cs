aaba9c2e5cc9fd9a65f1b2b99e87278fa4ca6fa8
qdjjx <qdjjx9441@sina.com>
2020-02-08 17:29:52 +0800

    日志基本面

---
+++ TotalNetCore.ByMrXiao.LoggingSimpleDemo/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TotalNetCore.ByMrXiao.LoggingSimpleDemo
+{
+    public class OrderService
+    {
+        private readonly ILogger<OrderService> _logger;
+
+        public OrderService(ILogger<OrderService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Show()
+        {
+            _logger.LogInformation($"show time: {DateTime.Now}");
+        }
+    }
+}
---
+++ TotalNetCore.ByMrXiao.LoggingSimpleDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace TotalNetCore.ByMrXiao.LoggingSimpleDemo
+{
+    class Program
+    {
+        public static object IServiceCollection { get; private set; }
+
+        static void Main(string[] args)
+        {
+            //IConfigurationBuilder用来创建IConfiguratioin
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder();
+            configBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange:true);
+            var config = configBuilder.Build();
+
+            //IServiceCollection用来注册服务
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IConfiguration>(t=>config);//工厂方式把配置放到容器里,IConfiguration被注入到容器
+
+            //AddLogging内含一些服务注册
+            //AddLogging是IServiceCollection的一个扩展方法
+            //把ILoggerFactory和LoggerFactory加入容器
+            //把ILogger和Logger放入容器
+            serviceCollection.AddLogging(builder => {
+                builder.AddConfiguration(config.GetSection("Logging"));
+                builder.AddConsole();
+            });
+
+            serviceCollection.AddTransient<OrderService>();
+
+            //IServiceProvider看作是容器
+            IServiceProvider service = serviceCollection.BuildServiceProvider();
+
+            var order = service.GetService<OrderService>();
+            order.Show();
+
+            ////ILoggerFactory用来生产ILogger
+            //ILoggerFactory loggerFactory = service.GetService<ILoggerFactory>();
+
+            //var aLogger = loggerFactory.CreateLogger("aLogger");
+            //aLogger.LogDebug(2001, "a");
+
+            //var bLogger = loggerFactory.CreateLogger("bLogger");
+            //bLogger.LogDebug("b");
+
+            //var logger = service.GetService<ILogger<Program>>();
+            //logger.LogInformation(new EventId(201,"c"),"hi");
+
+            Console.ReadKey();
+        }
+    }
+}
