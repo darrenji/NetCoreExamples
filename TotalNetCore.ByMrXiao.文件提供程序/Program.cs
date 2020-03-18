2a4e0c33afbbd75639e2bbf1048c0f5a85c581ca
qdjjx <qdjjx9441@sina.com>
2020-03-18 09:10:19 +0800

    访问任何位置的文件

---
+++ TotalNetCore.ByMrXiao.文件提供程序/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System;
+
+namespace TotalNetCore.ByMrXiao.文件提供程序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFileProvider provider1 = new PhysicalFileProvider(AppDomain.CurrentDomain.BaseDirectory);
+
+            //var contents = provider1.GetDirectoryContents("/");
+
+            #region 项目目录下的内容
+            //foreach(var item in contents)
+            //{
+            //    //读取文件流
+            //    //var fileStream = item.CreateReadStream();
+            //    Console.WriteLine(item.Name);
+            //}
+
+
+            #endregion
+
+            #region 读取嵌入式文件
+            IFileProvider provider2 = new EmbeddedFileProvider(typeof(Program).Assembly);
+            //var html = provider2.GetFileInfo("emb.html");
+            #endregion
+
+
+            #region 组合文件提供程序
+            IFileProvider provider = new CompositeFileProvider(provider1, provider2);
+            var contents = provider.GetDirectoryContents("/");
+
+            foreach (var item in contents)
+            {
+                Console.WriteLine(item.Name);
+            }
+            #endregion
+
+            //Console.ReadKey();
+
+        }
+    }
+}
